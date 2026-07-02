1dfd1ff344ece59c904eba0188da1562b2d27044
Bassant Farag <bassanttfarag28@gmail.com>
2026-07-02 18:50:28 +0300

    add login and register logic

---
+++ src/js/login.js
@@ -0,0 +1,55 @@
+const emailInput =document.getElementById("email");
+const passward =document.getElementById("password");
+const LoginForm =document.querySelector("form");
+
+LoginForm.addEventListener('submit',(e) =>{
+    e.preventDefault();
+     const emailValue=emailInput.value.trim();
+    const passwardValue=passward.value.trim();
+     if(emailValue==="" || passwardValue===""){
+         showNotification("Please fill in all fields!","error");
+         return;
+    }
+
+    let usersList=JSON.parse(localStorage.getItem("allusers"));
+    const foundUser=usersList.find((user) => user.email==emailValue)
+
+    if(!foundUser){
+        showNotification("this email don'y exist! pleaase register first .","error");
+    }else if(foundUser.passward!==passwardValue){
+        showNotification("incorrect password ! try again ","error");
+    }else{
+         showNotification("Registration successful! Redirecting...", "success");
+        setTimeout(()=>{
+            alert("🌸 Welcome back to Fleura!");
+            window.location.href="index.html"
+        },1000)
+    }
+    
+
+   
+})
+
+function showNotification(message ,type){
+    const message_div=document.createElement("div");
+    message_div.innerHTML=message;
+    message_div.className=`fixed bottom-5 left-1/2 -translate-x-1/2 px-6 py-3 z-50 opacity-0 rounded-xl text-white font-semibold shadow-lg transition-all duration-300 translate-y-10 `
+    document.body.appendChild(message_div);
+    if(type==="error"){
+        message_div.classList.add("bg-[#6E3B43]");
+    }else{
+        message_div.classList.add("bg-teal-900");
+    }
+
+    setTimeout(()=>{
+        message_div.classList.remove("translate-y-10" ,"opacity-0");
+
+    },100);
+
+    setTimeout(()=>{
+     message_div.classList.add("translate-y-10","opacity-0");
+     setTimeout(()=>{
+         message_div.remove();
+     },300)
+ },2500)
+}
--- src/js/register.js
+++ src/js/register.js
@@ -13,13 +13,58 @@ registerForm.addEventListener('submit' ,(e) => {
     const passwardValue=passward.value.trim();
 
     if(firstNameValue==="" || lastNameValue==="" || emailValue==="" || passwardValue===""){
-
+         showNotification("Please fill in all fields!","error");
+         return;
     }
+
+    const user={
+        id:Date.now(),
+        Firstname:firstNameValue,
+        lastName:firstNameValue,
+        email:emailValue,
+        passward:passwardValue
+    }
+
+    let usersList=JSON.parse(localStorage.getItem("allusers"))||[]
+    const currentEmail=usersList.some((user)=> user.email === emailValue);
+    if(currentEmail){
+        showNotification("this email is already registered ","error");
+        return;
+    }
+
+    usersList.push(user);
+    localStorage.setItem("allusers" ,JSON.stringify(usersList));
+    
+
+     setTimeout(() =>{
+         showNotification("Registration successful! Redirecting...", "success");
+        alert("🌷 Your garden is ready! Your Fleura account has been created.");
+        window.location.href="login.html"
+     },500)
+
 })
 
 
 function showNotification(message ,type){
     const message_div=document.createElement("div");
     message_div.innerHTML=message;
-    
+    message_div.className=`fixed bottom-5 left-1/2 -translate-x-1/2 px-6 py-3 z-50 opacity-0 rounded-xl text-white font-semibold shadow-lg transition-all duration-300 translate-y-10 `
+    document.body.appendChild(message_div);
+    if(type==="error"){
+        message_div.classList.add("bg-[#6E3B43]");
+    }else{
+        message_div.classList.add("bg-teal-900");
+    }
+
+    setTimeout(()=>{
+        message_div.classList.remove("translate-y-10" ,"opacity-0");
+
+    },100);
+
+    setTimeout(()=>{
+     message_div.classList.add("translate-y-10","opacity-0");
+     setTimeout(()=>{
+         message_div.remove();
+     },300)
+ },2500)
 }
